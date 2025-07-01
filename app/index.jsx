f776bc0e9436990a2321a504a352fcbd8a917b67
Zack uiui <metagorn.s@kkumail.com>
2025-07-01 11:17:05 +0700

    Update project structure: add Home component, modify layout, and set main entry point to expo-router.

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello world</Text>
+    </View>
+  );
+};
+
+export default Home; 
